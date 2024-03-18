349139d3cbbcbcd727774b874c2447e08e315f38
Thomas Zemp <tom@dhis2.org>
2024-03-18 13:34:48 +0100

    feat: add redirect for safe mode (#14)

---
+++ src/pages/safe-mode.js
@@ -0,0 +1,33 @@
+import i18n from '@dhis2/d2-i18n'
+import { Button } from '@dhis2/ui'
+import React, { useEffect } from 'react'
+import { useLoginConfig } from '../providers/index.js'
+
+const SAFE_MODE_ENDPOINT = 'dhis-web-commons/security/login.action'
+
+const SafeModePage = () => {
+    const { baseUrl, uiLocale } = useLoginConfig()
+    const safeURL = `${baseUrl}/${SAFE_MODE_ENDPOINT}`
+
+    useEffect(() => {
+        window.location.href = `${baseUrl}/${SAFE_MODE_ENDPOINT}`
+    }, [baseUrl])
+
+    return (
+        <>
+            <p>
+                {i18n.t(
+                    'You should shortly be redirected. If you are not redirected, please click redirect button.',
+                    { lng: uiLocale }
+                )}
+            </p>
+            <div>
+                <a rel="noopener noreferrer" href={safeURL}>
+                    <Button>Redirect</Button>
+                </a>
+            </div>
+        </>
+    )
+}
+
+export default SafeModePage
