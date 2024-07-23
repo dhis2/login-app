e9750fa8a85a7cbdaca9211da246e59fa4f9e974
Thomas Zemp <tom@dhis2.org>
2024-07-23 13:22:31 +0200

    fix: refactor errors

--- src/pages/login.js
+++ src/pages/login.js
@@ -26,6 +26,105 @@ export default function LoginPage() {
     )
 }
 
+const LoginErrors = ({
+    lngs,
+    error,
+    twoFAIncorrect,
+    passwordExpired,
+    passwordResetEnabled,
+    accountInaccessible,
+    unknownStatus,
+}) => {
+    if (error) {
+        return (
+            <FormNotice
+                title={
+                    !error?.details?.httpStatusCode ||
+                    error.details.httpStatusCode >= 500
+                        ? i18n.t('Something went wrong', {
+                              lngs,
+                          })
+                        : i18n.t('Incorrect username or password', {
+                              lngs,
+                          })
+                }
+                error
+            >
+                {(!error?.details?.httpStatusCode ||
+                    error.details.httpStatusCode >= 500) && (
+                    <span>{error?.message}</span>
+                )}
+            </FormNotice>
+        )
+    }
+
+    if (twoFAIncorrect) {
+        return (
+            <FormNotice
+                title={i18n.t('Incorrect authentication code', {
+                    lngs,
+                })}
+                error
+            />
+        )
+    }
+    if (passwordExpired) {
+        return (
+            <FormNotice
+                title={i18n.t('Password expired', {
+                    lngs,
+                })}
+                error
+            >
+                {passwordResetEnabled ? (
+                    <Link to="/reset-password">
+                        {i18n.t(
+                            'You can reset your from the password reset page.'
+                        )}
+                    </Link>
+                ) : (
+                    i18n.t('Contact your system administrator.')
+                )}
+            </FormNotice>
+        )
+    }
+    if (accountInaccessible) {
+        return (
+            <FormNotice
+                title={i18n.t('Account not accessible', {
+                    lngs,
+                })}
+                error
+            >
+                {i18n.t('Contact your system administrator.')}
+            </FormNotice>
+        )
+    }
+    if (unknownStatus) {
+        return (
+            <FormNotice
+                title={i18n.t('Something went wrong', {
+                    lngs,
+                })}
+                error
+            >
+                {i18n.t('Contact your system administrator.')}
+            </FormNotice>
+        )
+    }
+    return null
+}
+
+LoginErrors.propTypes = {
+    accountInaccessible: PropTypes.bool,
+    error: PropTypes.object,
+    lngs: PropTypes.arrayOf(PropTypes.string),
+    passwordExpired: PropTypes.bool,
+    passwordResetEnabled: PropTypes.bool,
+    twoFAIncorrect: PropTypes.bool,
+    unknownStatus: PropTypes.bool,
+}
+
 const InnerLoginForm = ({
     handleSubmit,
     formSubmitted,
@@ -161,72 +260,16 @@ const LoginForm = ({
 
     return (
         <>
-            {error && (
-                <FormNotice
-                    title={
-                        !error?.details?.httpStatusCode ||
-                        error.details.httpStatusCode >= 500
-                            ? i18n.t('Something went wrong', {
-                                  lngs,
-                              })
-                            : i18n.t('Incorrect username or password', {
-                                  lngs,
-                              })
-                    }
-                    error
-                >
-                    {(!error?.details?.httpStatusCode ||
-                        error.details.httpStatusCode >= 500) && (
-                        <span>{error?.message}</span>
-                    )}
-                </FormNotice>
-            )}
-            {twoFAIncorrect && (
-                <FormNotice
-                    title={i18n.t('Incorrect authentication code', {
-                        lngs,
-                    })}
-                    error
-                />
-            )}
-            {passwordExpired && (
-                <FormNotice
-                    title={i18n.t('Password expired', {
-                        lngs,
-                    })}
-                    error
-                >
-                    {passwordResetEnabled ? (
-                        <Link to="/reset-password">
-                            {i18n.t(
-                                'You can reset your from the password reset page.'
-                            )}
-                        </Link>
-                    ) : (
-                        i18n.t('Contact your system administrator.')
-                    )}
-                </FormNotice>
-            )}
-            {accountInaccessible && (
-                <FormNotice
-                    title={i18n.t('Account not accessible', {
-                        lngs,
-                    })}
-                    error
-                >
-                    {i18n.t('Contact your system administrator.')}
-                </FormNotice>
-            )}
-            {unknownStatus && (
-                <FormNotice
-                    title={i18n.t('Something went wrong', {
-                        lngs,
-                    })}
-                    error
-                >
-                    {i18n.t('Contact your system administrator.')}
-                </FormNotice>
-            )}
+            <LoginErrors
+                lngs={lngs}
+                error={error}
+                twoFAIncorrect={twoFAIncorrect}
+                passwordExpired={passwordExpired}
+                passwordResetEnabled={passwordResetEnabled}
+                accountInaccessible={accountInaccessible}
+                unknownStatus={unknownStatus}
+            />
+
             <ReactFinalForm.Form onSubmit={handleLogin}>
                 {({ handleSubmit }) => (
                     <InnerLoginForm
