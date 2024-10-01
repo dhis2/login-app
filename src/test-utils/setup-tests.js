c30055a94a60191729017a5dad5fc70bed6aa3d4
Thomas Zemp <tom@dhis2.org>
2024-10-01 14:55:29 +0200

    fix: handle undefined apiVersion from api/loginConfig ([LIBS-683] #49)

--- src/test-utils/setup-tests.js
+++ src/test-utils/setup-tests.js
@@ -1,7 +1,4 @@
 import '@testing-library/jest-dom'
-import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
 import * as matchers from 'jest-extended'
-expect.extend(matchers)
 
-configure({ adapter: new Adapter() })
+expect.extend(matchers)
